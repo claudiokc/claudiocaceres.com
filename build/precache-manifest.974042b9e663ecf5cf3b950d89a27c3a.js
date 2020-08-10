12b1dbf03093939d81ac658824ded4c8e87bc593
Claudio Caceres <claudio.caceres@admios-sa.com>
2020-08-09 21:02:35 -0600

    Create react app for frontend

---
+++ build/precache-manifest.974042b9e663ecf5cf3b950d89a27c3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84bccac3374121ba8490342c6625adc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "178a416cb546e008d164",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "38cd303266da803d29da",
+    "url": "/static/js/2.d89cdf29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d89cdf29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178a416cb546e008d164",
+    "url": "/static/js/main.53ed4494.chunk.js"
+  },
+  {
+    "revision": "415039f72dffc77dd458",
+    "url": "/static/js/runtime-main.1e8db441.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
